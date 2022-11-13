f1d900f0e3a1270d5884b00b5b4f6d522782b67b
costar011 <dnaer456@gmail.com>
2022-11-13 22:08:35 +0900

    html&css&js

---
+++ index.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector(".mainBtn");
+let mbtn = document.querySelector(".mianBtn");
+let pbtn = document.querySelector(".plusBtn");
+
+btn.addEventListener("click", () => {})
