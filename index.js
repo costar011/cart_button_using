59bc2cc72190b6bfbf14148caac5c6dc7a238c3c
costar011 <dnaer456@gmail.com>
2022-12-02 23:51:16 +0900

    js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 let btn = document.querySelector(".mainBtn");
 let mbtn = document.querySelector(".minusBtn");
 let pbtn = document.querySelector(".plusBtn");
+
+btn.addEventListener("click", () => {
+  if (btn.innerText == "ADD TO CART") {
+    btn.innerText = 1;
+    pbtn.style.display = "inline-block";
+    mbtn.style.display = "inline-block";
+  }
+});
+
+mbtn.addEventListener("click", () => {
+  if (btn.innerText == 5) {
+    pbtn.style.visibility = "hidden";
+  }
+});
